4da410654dcb93b31cd3b581d00cabe9800e1591
bart <bart@xengineering.co>
2018-09-20 14:16:28 +0900

    git init

---
+++ CMakeFiles/Source.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Documents/Workspace/sn3d-project/APP/main.c" "/home/pi/Documents/Workspace/sn3d-project/CMakeFiles/Source.dir/APP/main.c.o"
+  "/home/pi/Documents/Workspace/sn3d-project/MODUELS/MODUEL_HW_UART.c" "/home/pi/Documents/Workspace/sn3d-project/CMakeFiles/Source.dir/MODUELS/MODUEL_HW_UART.c.o"
+  "/home/pi/Documents/Workspace/sn3d-project/MODUELS/serial.c" "/home/pi/Documents/Workspace/sn3d-project/CMakeFiles/Source.dir/MODUELS/serial.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "./INCLUDE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
