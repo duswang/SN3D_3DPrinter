0f8a9a2f88e61c861bcfdf03ef2e9f783d4aaeba
bato <bart.xengienering.co>
2018-10-17 18:31:34 +0900

    ADD APP CONTROL .h and SN_ERROR.h

---
+++ APP/INCLUDE/APP_CONTROL.h
@@ -0,0 +1,14 @@
+/*
+ * APP_CONTROL.h
+ *
+ *  Created on: Oct 17, 2018
+ *      Author: pi
+ */
+
+#ifndef APP_CONTROL_H_
+#define APP_CONTROL_H_
+
+extern int APP_CONTROL_EvtHdlr(general_evt_t evt);
+
+
+#endif /* APP_CONTROL_H_ */
